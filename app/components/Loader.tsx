3089bd2bdb163cfda77fad3c5a56ab084c6269c8
robinlrx <robin@monkey-lab.io>
2025-02-22 21:33:15 +0100

    add mb to loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loader() {
 	return (
-		<div className='flex space-x-2'>
+		<div className='flex space-x-2 mt-4'>
 			<div className="size-3 rounded-full bg-neutral-300 animate-bounce"></div>
 			<div className="size-3 rounded-full bg-neutral-300 animate-bounce"></div>
 			<div className="size-3 rounded-full bg-neutral-300 animate-bounce"></div>
